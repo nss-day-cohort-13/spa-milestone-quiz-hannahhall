13af7f1c8db7b79190132cc49e1677dd2d1f788e
Hannah Hall <hannahkhall13@gmail.com>
2016-05-08 13:53:11 -0500

    added more cars to json file, finished styling, commented code

--- activateEvents.js
+++ activateEvents.js
@@ -6,7 +6,7 @@ var CarLot 			= (function (events) {
 	var description;
 	var id;
 
-// When car element is selected, the text input auto populates with the description of the selected car
+	// When car element is selected, the text input auto populates with the description of the selected car
 	function textInputFocus (event) {
 		var currentSelection = event.target.parentNode;
 		if (currentSelection.tagName.toLowerCase()=== "section") {
@@ -17,12 +17,12 @@ var CarLot 			= (function (events) {
 		}
 	}
 
-//As the user types the selected cars description matches what is being typed in the input field
+	//As the user types the selected cars description matches what is being typed in the input field
 	function replace () {
 		description.innerHTML = textInput.value;
 	}
 
-// When the update button is clicked the information in the Inventory array is changed to match the updated text
+	// When the update button is clicked the information in the Inventory array is changed to match the updated text
 	function updateDescription () {
 		CarLot.getInventory()[id].description = textInput.value;
 		textInput.value = "";
@@ -34,7 +34,6 @@ var CarLot 			= (function (events) {
 			updateBtn.addEventListener("click", updateDescription);
 			updateBtn.addEventListener("click", CarLot.toggleOff);
 			textInput.addEventListener("keyup", replace);
-
 		}
 
 	return events
--- quiz.js
+++ quiz.js
@@ -1,6 +1,7 @@
 function populatePage (inventory) {
 	var pageInput = document.getElementById("inventory");
 	var data= "";
+
 	for(i=0; i<inventory.length; i++) {
 		data+= `<section class='col-md-4 ${i} ${inventory[i].color}'>
 						<h2>${inventory[i].make} ${inventory[i].model}</h2>
@@ -10,16 +11,15 @@ function populatePage (inventory) {
 						</p>
 						<p>Purchased: ${inventory[i].purchased}</p>
 						<p id='${i}' class='description'>${inventory[i].description}</p></section>`;
+					}
 	pageInput.innerHTML= data;
+
+	// Adds colored border around car elements
 	var section = document.getElementsByClassName("col-md-4");
 	for(j=0; j<section.length; j++) {
-		section[j].style.border = `1px solid ${inventory[j].color}`;
+		section[j].style.border = `2px solid ${inventory[j].color}`;
 	}
-}
-	CarLot.activateEvents();
 
+	CarLot.activateEvents();
 }
 CarLot.loadInventory(populatePage);
-
-
-//		document.getElementById(`${inventory[i].color}`).style.border = ``;
