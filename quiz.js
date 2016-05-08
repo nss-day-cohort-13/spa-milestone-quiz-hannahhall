849b3f61b4835eba82f252e684a896caeb70736a
Hannah Hall <hannahkhall13@gmail.com>
2016-05-07 20:59:21 -0500

    added js styling for border color

--- quiz.js
+++ quiz.js
@@ -1,19 +1,25 @@
 function populatePage (inventory) {
 	var pageInput = document.getElementById("inventory");
 	var data= "";
-	data= `<div class= col-md-1></div>`
 	for(i=0; i<inventory.length; i++) {
-		data+=`	<section class='col-md-3 ${inventory[i].color}'>
+		data+= `<section class='col-md-4 ${i} ${inventory[i].color}'>
 						<h2>${inventory[i].make} ${inventory[i].model}</h2>
 						<p>	Year: ${inventory[i].year}
 								Price: ${inventory[i].price}
 								Color: ${inventory[i].color}
 						</p>
 						<p>Purchased: ${inventory[i].purchased}</p>
-						<p id='${i}' class='description'>${inventory[i].description}</p></section>`
-	}
-	data += `<div class= col-md-1></div>`
+						<p id='${i}' class='description'>${inventory[i].description}</p></section>`;
 	pageInput.innerHTML= data;
+	var section = document.getElementsByClassName("col-md-4");
+	for(j=0; j<section.length; j++) {
+		section[j].style.border = `1px solid ${inventory[j].color}`;
+	}
+}
 	CarLot.activateEvents();
- }
+
+}
 CarLot.loadInventory(populatePage);
+
+
+//		document.getElementById(`${inventory[i].color}`).style.border = ``;
